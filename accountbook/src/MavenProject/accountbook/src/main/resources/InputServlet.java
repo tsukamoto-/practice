be47d4316aab5eab454b1062409fd6490e3ebf46
tsukamoto- <tsukamoto@fogacom.com>
2013-11-04 22:59:56 +0900

    Maven Project で作り直して、買い物内容入力サーブレットを作成

---
+++ accountbook/src/MavenProject/accountbook/src/main/resources/InputServlet.java
@@ -0,0 +1,44 @@
+package com.fogacom.tsukamoto.accountbook
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class InputServlet
+ */
+public class InputServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public InputServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html; charset=utf-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Input</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("”ƒ‚¢•¨");
+		out.println("</body>");
+		out.println("</html>");
+
+	}
+
+}
